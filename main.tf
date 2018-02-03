f2cfb0456075db867c723aab95ed4aa6e5568b52
mveguillas <miguel@veguillas.com>
2018-02-04 00:05:16 +0100

    3

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "example" {
   ami           = "ami-5652ce39"
   instance_type = "t2.micro"
   tags {
-    Name = "terraform-example2"
+    Name = "terraform-example3"
   }
 }
 
