37899079cb6a2de8f3699a543cdfb5857ee04edc
mveguillas <miguel@veguillas.com>
2018-02-03 23:26:46 +0100

    Initial Commit

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-5652ce39"
+  instance_type = "t2.micro"
+  tags {
+    Name = "terraform-example"
+  }
+}
