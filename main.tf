fb3080a7458aa4b9db9da1295ff7aaf5fd6d7907
mveguillas <miguel@veguillas.com>
2018-02-04 11:25:26 +0100

    fixed user data, variables

--- main.tf
+++ main.tf
@@ -2,14 +2,17 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-5652ce39"
-  instance_type = "t2.micro"
+resource "aws_instance" "instance" {
+  ami                    = "ami-af79ebc0"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
 
   user_data = <<-EOF
     #!/bin/bash
+    echo "----user_data start"
     echo "hola caracola" > index.html
-    nohup busybox httpd -f -p 8080 &
+    nohup busybox httpd -f -p "${var.server_port}" &
+    echo "----user_data stop"
     EOF
 
   tags {
@@ -17,13 +20,23 @@ resource "aws_instance" "example" {
   }
 }
 
-resource "aws.aws_security_group" "instance" {
+resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = "${var.server_port}"
+    to_port     = "${var.server_port}"
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+variable "server_port"{
+  description = "port for the web server"
+  default = 8080
+}
+output "public_ip"{
+  value = "${aws_instance.instance.public_ip}"
+}
+output "public_dns"{
+  value = "${aws_instance.instance.public_dns}"
 }
